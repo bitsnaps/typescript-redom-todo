99c03f80d0bb290fecf6aa40b4ad712f198bde2e
Ibrahim.H <bitsnaps@yahoo.fr>
2022-03-31 20:03:13 +0100

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { el, mount, unmount } from "redom";
 import Adding from "./adding";
 
 var h1: object = el("h1", "Hello RE:DOM!");
-var logo: object = el("h1", "Hello RE:DOMsddddff''fd!");
+var logo: object = el("h1", "Hello RE:DOM");
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById("app");
 //appDiv.innerHTML = h1.innerHTML;
